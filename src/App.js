43d8d2452468b370beca8a98b719e4dd28cb1512
Christian Studer <christian.studer2@swisscom.com>
2019-07-19 14:30:30 +0200

    React: Installs routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,46 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function AppRouter() {
   return (
     <Container>
-      <div>Quo bike is</div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </div>
+      </Router>
     </Container>
   );
 }
 
-export default App;
+export default AppRouter;
