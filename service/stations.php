802bc7b134c8db3e9a4eda41c7152ef189b75aa4
Christian Studer <cstuder@existenz.ch>
2019-07-20 06:59:44 +0200

    WIP: Service

---
+++ service/stations.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Fetch stations details from PubliBike
+ *
+ * @param int $id
+ * @author Christian Studer <cstuder@existenz.ch>
+ */
+
+// Configuation
+define('PUBLIBIKEAPI', 'https://api.publibike.ch/v1/public/');
+
+$id = $_GET['id'] ?? null;
+$idclean = intval($id);
+
+// Fetch data
+$station = file_get_contents(PUBLIBIKEAPI . 'stations/' . $idclean);
+
+// Check for error
+if($station === FALSE) {
+    http_response_code(400);
+    exit();
+}
+
+// Output response
+header('Content-type: application/json');
+echo $station;
