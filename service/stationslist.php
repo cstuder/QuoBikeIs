0c7c2ae9120d97306bac142e01fb05d12abfcd4c
Christian Studer <christian.studer2@swisscom.com>
2019-07-30 17:57:36 +0200

    Stationslist: Gets name and city from stations.php service

--- service/stationslist.php
+++ service/stationslist.php
@@ -7,7 +7,10 @@
 
 // Configuation
 define('PUBLIBIKEAPI', 'https://api.publibike.ch/v1/public/');
+define('CACHEDIR', realpath(__DIR__ . '/cache') . '/');
 
+//define('STATIONSSERVICE', str_replace('stationslist.php', "stations.php", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"));
+define('STATIONSSERVICE', 'http://localhost:8001/stations.php');
 // Fetch data
 $stationslist = file_get_contents(PUBLIBIKEAPI . 'stations');
 
@@ -19,7 +22,28 @@ if($stationslist === FALSE) {
     http_response_code(400);
     exit();
 }
+ini_set('max_execution_time', 300); 
+// Gather the station names for all stations
+$stations = json_decode($stationslist);
+
+foreach($stations as $index => $station) {
+    $stationdataRaw = file_get_contents(CACHEDIR . "{$station->id}.json");
+
+    if($stationdataRaw === FALSE || ($stationdata = json_decode($stationdataRaw)) === FALSE) {
+        // Fetch station data from the stations service, wherever it is
+        $stationdataRaw = file_get_contents(STATIONSSERVICE . "?id={$station->id}");
+
+        if($stationdataRaw === FALSE || ($stationdata = json_decode($stationdataRaw)) === FALSE) {
+            // Neither cache nor service did work, set the name manually
+            $stationdata = (object) ['name' => $station->id, 'city' => '???'];
+        }
+    }
+
+    // Augment answer
+    $stations[$index]->name = $stationdata->name;
+    $stations[$index]->city = $stationdata->city;
+}
 
 // Output response
 header('Content-type: application/json');
-echo $stationslist;
+echo json_encode($stations);
