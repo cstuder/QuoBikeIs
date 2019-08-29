9aa20edefe86bf9bd8974e856fac57717d337e9c
Christian Studer <cstuder@existenz.ch>
2019-08-29 22:22:34 +0200

    Deploy: Typos

--- deploy/action.sh
+++ deploy/action.sh
@@ -22,9 +22,9 @@ SRC_SERVICE_PATH="$GITHUB_WORKSPACE/service/";
 
 ENV_FILE="$GITHUB_WORKSPACE/.env.local"
 LOCALAPI_LIVE="https://quobikeis.existenz.ch/service/"
-LOCALAPI_TEST="https://quobiekis-test.existenz.ch/service"
+LOCALAPI_TEST="https://quobikeis-test.existenz.ch/service"
 
-NOTIFICATION_TITLE="aare.guru.api {{ ref }} deployed"
+NOTIFICATION_TITLE="quobikeis {{ ref }} deployed"
 NOTIFICATION_BODY="Commit by {{ head_commit.author.name }}: {{ head_commit.message | truncate(128) }} ({{ head_commit.id[0:7] }})"
 NOTIFICATION_URL="$APPRISE_URL"
 
@@ -37,7 +37,7 @@ REF=$(cat $GITHUB_EVENT_PATH | jq '.ref')
 if [[ "$REF" =~ \/([^\/]*)\"$ ]]; then
   BRANCH="${BASH_REMATCH[1]}"
   echo "Branch detected: $BRANCH"
-  NOTIFICATION_TITLE="aare.guru.api $BRANCH deployed"
+  NOTIFICATION_TITLE="quobikeis $BRANCH deployed"
 else
   echo "No branch found in ref: $REF"
   exit 1
@@ -76,8 +76,6 @@ SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
 echo "$SSH_PRIVATE_KEY" > "$SSH_PATH/id_rsa"
 chmod 600 "$SSH_PATH/id_rsa"
-
-ls -la $SSH_PATH
 
 # Execute rsync
 rsync --progress --verbose --archive -e 'ssh -o StrictHostKeyChecking=no -i '"$SSH_PATH"'/id_rsa' $SRC_PATH $DEST
