11e41cc0b3ba5630ce53fc44247ffdd94740fae0
Christian Studer <christian.studer2@swisscom.com>
2019-08-29 16:55:08 +0200

    Localapi: Take value from environment

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@
 export const PUBLIBIKEAPI = "https://api.publibike.ch/v1/public/";
 export const PUBLIBIKE_ID_BIKE = 1;
 export const PUBLIBIKE_ID_EBIKE = 2;
-export const LOCALAPI = "http://localhost:8000/"; // TODO make this flexible, get if from the environment or so...
+export const LOCALAPI = process.env.REACT_APP_LOCALAPI;
